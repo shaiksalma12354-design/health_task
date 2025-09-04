379a23ee3245cd7e0718966dffab643c9e2a00d4
shaiksalma12354-design <shaiksalma12354@gmail.com>
2025-09-04 13:46:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # ---------- GITHUB RAW FILE LINKS ----------
 DISEASES_URL = "https://raw.githubusercontent.com/shaiksalma12354-design/health_task/main/diseases.json"
-SYMPTOMS_URL = 
+SYMPTOMS_URL = "https://raw.githubusercontent.com/shaiksalma12354-design/health_task/main/symptoms.json"
 PREVENTIONS_URL = "https://raw.githubusercontent.com/shaiksalma12354-design/health_task/main/preventions.json"
 
 
